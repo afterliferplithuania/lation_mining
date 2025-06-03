05e9b5a41c65a287e33e638263bb56c443617b25
lsrplt <ringysp@gmail.com>
2025-06-03 17:27:00 +0300

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -126,3 +126,16 @@ function PlayerLog(source, title, message)
         {['Content-Type'] = 'application/json'})
     end
 end
+
+MySQL.query([[
+CREATE TABLE IF NOT EXISTS `lation_mining` (
+  `identifier` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `level` int(11) NOT NULL DEFAULT 1,
+  `exp` int(11) NOT NULL DEFAULT 0,
+  `mined` int(11) NOT NULL DEFAULT 0,
+  `smelted` int(11) NOT NULL DEFAULT 0,
+  `earned` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`identifier`)
+);
+]])
